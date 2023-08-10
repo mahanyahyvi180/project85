49fd46c6c05b28bbcc9587cf3dedcdab3545fc27
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 15:37:54 +0330

    Add files via upload

---
+++ ConsoleApp85/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int sum;
+
+            Console.Write("Enter first integer :");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second integer:");
+            number2 = int.Parse(Console.ReadLine());
+            sum = number1 + number2;
+
+            Console.WriteLine($"Sum is{sum}");
+            Console.ReadLine();
+        }
+    }
+}
